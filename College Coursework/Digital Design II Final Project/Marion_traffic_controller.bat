e9f2d09d560f8cc873de10e8cddff22d8f66901d
FPGADude <david.marion74@yahoo.com>
2021-11-02 08:30:46 -0600

    Create Marion_traffic_controller.bat

---
+++ College Coursework/Digital Design II Final Project/Marion_traffic_controller.bat
@@ -0,0 +1,1 @@
+iverilog -o traffic_controller traffic_controller.v traffic_controller_TB.v light_control.v timer_mode.v sensor_mode.v counter1.v counter2.v counter3.v counter4.v counter5.v counter6.v light_fsm.v light_fsm2.v light_decoder.v light_decoder2.v srl.v wdwt_decoder_Maj.v wdwt_decoder_min.v wdws_decoder.v
