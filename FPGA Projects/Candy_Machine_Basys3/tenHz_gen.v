ea828e6232e5f3b9ed8f1e3f2433d75a1cc39984
FPGADude <david.marion74@yahoo.com>
2022-02-17 16:23:14 -0700

    Create tenHz_gen.v

---
+++ FPGA Projects/Candy_Machine_Basys3/tenHz_gen.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/16/2022 02:52:38 PM
+// Design Name: 
+// Module Name: tenHz_gen
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tenHz_gen(
+
+    );
+endmodule
