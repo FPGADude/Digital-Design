5792389821cd0f505767e5ed61103a92cedb8752
FPGADude <david.marion74@yahoo.com>
2022-03-31 15:40:19 -0600

    Create top.v

---
+++ FPGA Projects/VGA Projects/VGA Text Generation/top.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+module top(
+    input clk,          // 100MHz on Basys 3
+    input reset,        // btnC on Basys 3
+    output hsync,       // to VGA connector
+    output vsync,       // to VGA connector
+    output [11:0] rgb   // to DAC, to VGA connector
+    );
+    
+    // signals
+    wire [9:0] w_x, w_y;
+    wire w_video_on, w_p_tick;
+    reg [11:0] rgb_reg;
+    wire [11:0] rgb_next;
+    
+    // VGA Controller
+    vga_controller vga(.clk_100MHz(clk), .reset(reset), .hsync(hsync), .vsync(vsync),
+                       .video_on(w_video_on), .p_tick(w_p_tick), .x(w_x), .y(w_y));
+    // Text Generation Circuit
+    ascii_test at(.clk(clk), .video_on(w_video_on), .x(w_x), .y(w_y), .rgb(rgb_next));
+    
+    // rgb buffer
+    always @(posedge clk)
+        if(w_p_tick)
+            rgb_reg <= rgb_next;
+            
+    // output
+    assign rgb = rgb_reg;
+      
+endmodule
