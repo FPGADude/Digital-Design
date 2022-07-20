027e66e30136a2cfea1f1ee67a005a75f5fa8b81
FPGADude <david.marion74@yahoo.com>
2022-07-20 12:35:24 -0600

    Create clkgen_200kHz.v

---
+++ FPGA Projects/NexysA7_Temp_Sensor_I2C/clkgen_200kHz.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+// Created by David J. Marion
+// Date 7.19.2022
+// 200kHz Generator for the Nexys A7 Temperature Sensor I2C Master
+
+module clkgen_200kHz(
+    input clk_100MHz,
+    output clk_200kHz
+    );
+    
+    // 100 x 10^6 / 200 x 10^3 / 2 = 250 <-- 8 bit counter
+    reg [7:0] counter = 8'h00;
+    reg clk_reg = 1'b1;
+    
+    always @(posedge clk_100MHz) begin
+        if(counter == 249) begin
+            counter <= 8'h00;
+            clk_reg <= ~clk_reg;
+        end
+        else
+            counter <= counter + 1;
+    end
+    
+    assign clk_200kHz = clk_reg;
+    
+endmodule
